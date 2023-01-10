6f7952e0fff4025fae2c66836562003fbe1900db
LaisFSGomes <laisgomes@alu.ufc.br>
2023-01-10 19:00:24 -0300

    feat: criação do componente AppBar

---
+++ src/components/AppBar/AppBar.styles.ts
@@ -0,0 +1,81 @@
+import { Box, IconButton, Menu } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const AppBarWrapper = styled(Box)({
+  height: "50px",
+  background: "#49006A",
+  display: "flex",
+  justifyContent: "flex-end",
+  padding: "0 20px",
+  "@media (max-width: 600px)": {
+    height: "40px",
+  },
+});
+export const AppBarComputer = styled(Box)({
+  width: "80%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  "@media (max-width: 940px)": {
+    width: "90%",
+  },
+  "@media (max-width: 775px)": {
+    display: "none",
+  },
+});
+export const InfoWrapper = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "50px",
+  "@media (max-width: 1300px)": {
+    gap: "30px",
+  },
+  "@media (max-width: 1100px)": {
+    gap: "20px",
+  },
+  "@media (max-width: 860px)": {
+    gap: "10px",
+  },
+});
+export const SectionsTypography = styled("a")({
+  fontSize: "15px",
+  fontWeight: 550,
+  color: "#FFFFFF",
+  cursor: "pointer",
+  textTransform: "uppercase",
+  fontFamily: "Poppins, sans-serif",
+  "&:hover": {
+    color: "#BFB7B7",
+  },
+  "@media (max-width: 1300px)": {
+    fontSize: "16px",
+    letterSpacing: "0.05em",
+  },
+  "@media (max-width: 1000px)": {
+    fontSize: "14px",
+    letterSpacing: "0.01em",
+  },
+  "@media (max-width: 775px)": {
+    color: "#49006A",
+    "&:hover": {
+        color: "#601B81",
+      },
+  },
+});
+export const AppBarMobile = styled(Box)({
+  display: "none",
+  "@media (max-width: 775px)": {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+});
+export const ButtonBar = styled(IconButton)({
+  "& svg": {
+    color: "#FFFFFF",
+  },
+  "&:hover": {
+    background: "transparent",
+  },
+});
+export const MenuMobile = styled(Menu)({});
---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { MenuItem } from "@mui/material";
+import {
+  AppBarComputer,
+  AppBarMobile,
+  AppBarWrapper,
+  ButtonBar,
+  MenuMobile,
+  SectionsTypography,
+} from "./AppBar.styles";
+import MenuIcon from "@mui/icons-material/Menu";
+export const AppBar: React.FC = () => {
+  const sections = [
+    "Curriculo",
+    "Sobre Mim",
+    "Habilidades",
+    "Projetos",
+    "Entre em Contato",
+    "Onde me encontrar",
+  ];
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <AppBarWrapper>
+      <AppBarComputer>
+        {sections.map((section, index) => {
+          return <SectionsTypography key={index}>{section}</SectionsTypography>;
+        })}
+      </AppBarComputer>
+      <AppBarMobile>
+        <ButtonBar onClick={handleClick}>
+          <MenuIcon />
+        </ButtonBar>
+        <MenuMobile
+          id="menu-mobile"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        > 
+        {sections.map((section, index) => {
+          return (
+            <MenuItem key={index} >
+                <SectionsTypography>{section}</SectionsTypography>
+            </MenuItem>
+          )
+        })}
+        </MenuMobile>
+      </AppBarMobile>
+    </AppBarWrapper>
+  );
+};
